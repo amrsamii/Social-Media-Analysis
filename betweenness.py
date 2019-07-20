bc468e91eb568474aef0e0a66926844a98f4fc3e
MohamedAdel70 <eslamsolari1996@gmail.com>
2019-07-20 14:49:43 +0200

    Simulation for centrality algorithmusing networkx

---
+++ betweenness.py
@@ -0,0 +1,12 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+g=nx.MultiGraph();
+g=nx.read_edgelist("eslam.txt",create_using=nx.MultiGraph(),nodetype=int,data=[('weight',int)])
+
+
+d= dict(nx.betweenness_centrality(g))
+weight=nx.get_edge_attributes(g,'weight')
+nx.draw_networkx(g,nodelist=d.keys(),node_size=[v*5000 for v in d.values()],edge_labels=weight,with_labels=True)
+
+plt.show()
